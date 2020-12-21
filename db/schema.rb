fb029534a536b048ec853cb02623bca251120e77
vagrant <vagrant@localhost.localdomain>
2020-12-21 13:37:12 +0000

    [Add]category:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_143019) do
+ActiveRecord::Schema.define(version: 2020_12_21_133341) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "category_name"
+    t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
