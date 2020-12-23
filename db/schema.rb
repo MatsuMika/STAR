12fef7c4c8ccedc385e865e5a86bf83bb49e4284
vagrant <vagrant@localhost.localdomain>
2020-12-23 16:01:12 +0000

    [Add]seller:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_152234) do
+ActiveRecord::Schema.define(version: 2020_12_23_150004) do
 
   create_table "cart_products", force: :cascade do |t|
     t.integer "product_id"
@@ -49,6 +49,22 @@ ActiveRecord::Schema.define(version: 2020_12_22_152234) do
     t.string "image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sellers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name", default: "", null: false
+    t.string "postal_code", default: "", null: false
+    t.string "address", default: "", null: false
+    t.string "phone_number", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_sellers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_sellers_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
