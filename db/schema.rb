71710e7c8b7fa72e7d43348536b5871342df7637
vagrant <vagrant@localhost.localdomain>
2020-12-24 03:36:56 +0000

    [Add]address:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_032712) do
+ActiveRecord::Schema.define(version: 2020_12_24_033441) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.string "address"
+    t.string "post_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "cart_products", force: :cascade do |t|
     t.integer "product_id"
