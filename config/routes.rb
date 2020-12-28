a7296071b4a71a43bf296348b0c17448fa5f2167
vagrant <risa.mkwork314@gmail.com>
2020-12-28 12:52:10 +0000

    [Add]application.controller:sign_in/resource,routes:divise,root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,34 @@
 Rails.application.routes.draw do
+
+	devise_for :sellers, controllers: {
+    sessions: 'sellers/sessions',
+    passwords: 'sellers/passwords',
+    registrations: 'sellers/registrations'
+  }
+
+  namespace :admins do
+    root 'users#top'
+    resources :users, only: [:index, :show, :edit, :update]
+  end
+
+
+	root 'users/products#top'
+	get 'users/about' => 'users/products#about'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+ 	}
+
+ 	scope module: :users do
+    get 'users/my_page' => 'users#show'
+    get 'users/edit' => 'users#edit'
+    patch 'users' => 'users#update'
+    delete 'cart_products/destroy_all' => 'cart_products#destroy_all'
+    resources :products, only: [:index, :show]
+  end
+
   get 'order_items/update'
   get 'categories/index'
   get 'categories/create'
@@ -14,12 +44,6 @@ Rails.application.routes.draw do
   get 'cart_products/create'
   get 'cart_products/update'
   get 'cart_products/destroy'
-  devise_for :sellers
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-	root 'products#top'
-
 
 
 end
