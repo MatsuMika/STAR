2c09de7c9b95e3a3e606f7d87db6468f05cbe41b
vagrant <risa.mkwork314@gmail.com>
2020-12-29 12:34:33 +0000

    [Add]view:divise:users,sellers

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     registrations: 'sellers/registrations'
   }
 
-  namespace :admins do
+  namespace :sellerss do
     root 'users#top'
     resources :users, only: [:index, :show, :edit, :update]
   end
@@ -29,21 +29,7 @@ Rails.application.routes.draw do
     resources :products, only: [:index, :show]
   end
 
-  get 'order_items/update'
-  get 'categories/index'
-  get 'categories/create'
-  get 'categories/edit'
-  get 'categories/update'
-  get 'categories/destroy'
-  get 'addresses/index'
-  get 'addresses/create'
-  get 'addresses/edit'
-  get 'addresses/update'
-  get 'addresses/destroy'
-  get 'cart_products/index'
-  get 'cart_products/create'
-  get 'cart_products/update'
-  get 'cart_products/destroy'
+
 
 
 end
