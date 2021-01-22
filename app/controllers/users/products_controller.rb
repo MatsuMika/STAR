4d9f29d4dd8234095cc6f80000cd2e2a4049e3bc
vagrant <risa.mkwork314@gmail.com>
2021-01-22 14:12:36 +0000

    users:products/index

--- app/controllers/users/products_controller.rb
+++ app/controllers/users/products_controller.rb
@@ -1,12 +1,20 @@
 class Users::ProductsController < ApplicationController
 
 	def top
+		@categories = Category.all
+		@products = Product.all
 	end
 
 	def about
 	end
 
   def index
+  	@producrs = Product.all
+  	@categories = Category.all
+  	if params[:category_id]
+  	 	@category = Category.find(params[:category_id])
+  	 	@products = @products.where(category_id: params[:category_id])
+  	end
   end
 
   def show
